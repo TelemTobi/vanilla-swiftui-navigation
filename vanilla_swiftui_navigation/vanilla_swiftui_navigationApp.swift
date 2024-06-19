e7ce9dad0e50db706109a40b76f17734b4437ee9
Telem Tobi <telem16@gmail.com>
2024-06-19 20:41:07 +0300

    Initial Commit

---
+++ vanilla_swiftui_navigation/vanilla_swiftui_navigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  vanilla_swiftui_navigationApp.swift
+//  vanilla_swiftui_navigation
+//
+//  Created by Telem Tobi on 19/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct vanilla_swiftui_navigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
